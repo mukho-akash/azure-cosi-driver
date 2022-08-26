ea5b1fefee668d1cd688032ca6ebee88e591dbd1
aarongalang <aarongalang@microsoft.com>
2022-08-26 12:57:11 -0400

    added more constants for unit testing

--- pkg/constant/mock.go
+++ pkg/constant/mock.go
@@ -1,12 +1,15 @@
 package constant
 
 const (
-	ValidAccount      = "validaccount"
-	InvalidAccount    = "invalidaccount"
-	ValidContainer    = "validcontainer"
-	InvalidContainer  = "invalidcontainer"
-	ValidBlob         = "validblob"
-	ValidBlobURL      = "https://validaccount.blob.core.windows.net/validcontainer/validblob"
-	ValidContainerURL = "https://validaccount.blob.core.windows.net/validcontainer"
-	ValidAccountURL   = "https://validaccount.blob.core.windows.net/"
+	ValidAccount       = "validaccount"
+	InvalidAccount     = "invalidaccount"
+	ValidContainer     = "validcontainer"
+	InvalidContainer   = "invalidcontainer"
+	ValidAccountType   = "standardgeneral-purposev2"
+	ValidBlob          = "validblob"
+	ValidResourceGroup = "resourcegroup"
+	ValidRegion        = "centralus"
+	ValidBlobURL       = "https://validaccount.blob.core.windows.net/validcontainer/validblob"
+	ValidContainerURL  = "https://validaccount.blob.core.windows.net/validcontainer"
+	ValidAccountURL    = "https://validaccount.blob.core.windows.net/"
 )
