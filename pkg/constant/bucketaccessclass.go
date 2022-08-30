d52fef2bcb5a6826184c856fa1cc22ef9574fee1
aarongalang <aarongalang@microsoft.com>
2022-08-30 15:01:35 -0400

    updating bucketaccessclass

--- pkg/constant/bucketaccessclass.go
+++ pkg/constant/bucketaccessclass.go
@@ -2,71 +2,16 @@ package constant
 
 const (
 	//BucketAccessClassFields
-	SignedVersionField      = "signedversion"
-	SignedPermissionsField  = "signedpermissions"
-	SignedExpiryField       = "signedexpiry"
-	SignedResourceTypeField = "signedresourcetype"
+	SignedVersionField                    = "signedversion"
+	SignedExpiryField                     = "signedexpiry"
+	SignedIPField                         = "signedipfield"
+	SignedStartField                      = "signedstart"
+	SignedProtocolField                   = "signedprotocol"
+	EnableListField                       = "enablelist"
+	EnableReadField                       = "enableread"
+	EnableWriteField                      = "enablewrite"
+	EnablePermanentDeleteField            = "enablepermanentdelete"
+	AllowServiceSignedResourceTypeField   = "allowservicesignedresourcetypefield"
+	AllowContainerSignedResourceTypeField = "allowcontainersignedresourcetypefield"
+	AllowObjectSignedResourceTypeField    = "allowobjectsignedresourcetypefield"
 )
-
-type SignedResourceType int
-type SignedPermissions int
-
-const (
-	TypeObject SignedResourceType = iota
-	TypeContainer
-	TypeObjectAndContainer
-)
-
-const (
-	Read SignedPermissions = iota
-	Add
-	Create
-	Write
-	Delete
-	ReadWrite
-	AddDelete
-	List
-	DeleteVersion
-	PermanentDelete
-	All
-)
-
-func (s SignedResourceType) String() string {
-	switch s {
-	case TypeObject:
-		return "object"
-	case TypeContainer:
-		return "container"
-	case TypeObjectAndContainer:
-		return "objectandcontainer"
-	}
-	return "unknown"
-}
-
-func (s SignedPermissions) String() string {
-	switch s {
-	case Read:
-		return "read"
-	case Add:
-		return "add"
-	case Create:
-		return "create"
-	case Write:
-		return "write"
-	case Delete:
-		return "delete"
-	case ReadWrite:
-		return "readwrite"
-	case AddDelete:
-		return "adddelete"
-	case List:
-		return "list"
-	case DeleteVersion:
-		return "deleteversion"
-	case PermanentDelete:
-		return "permanentdelete"
-	case All:
-		return "all"
-	}
-	return "unknown"
-}
