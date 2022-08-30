1c49a12532116165ea682b09ea276fa4b7a338f0
aarongalang <aarongalang@microsoft.com>
2022-08-30 15:18:02 -0400

    moved SAS creation to it's own file in access_ops.go, partially for organization, and partially because container_ops uses a different azblob package

--- pkg/azureutils/storage_account_ops.go
+++ pkg/azureutils/storage_account_ops.go
@@ -16,9 +16,7 @@ package azureutils
 import (
 	"context"
 	"fmt"
-	"time"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	azure "sigs.k8s.io/cloud-provider-azure/pkg/provider"
@@ -45,50 +43,4 @@ func createStorageAccountBucket(ctx context.Context,
 		return "", status.Error(codes.Internal, fmt.Sprintf("Could not create storage account: %v", err))
 	}
 	return accName, nil
-}
-
-//creates SAS and returns service client with sas
-func createAccountSASURL(ctx context.Context, account string, parameters BucketAccessClassParameters, cloud *azure.Cloud) (string, error) {
-	cred, err := azblob.NewSharedKeyCredential(account, "myAccountKey")
-	if err != nil {
-		return "", err
-	}
-	serviceURL := fmt.Sprintf("https://%s.blob.core.windows.net", account)
-	serviceClient, err := azblob.NewServiceClientWithSharedKey(serviceURL, cred, nil)
-	if err != nil {
-		return "", err
-	}
-
-	resources := azblob.AccountSASResourceTypes{}
-	if parameters.allowServiceSignedResourceType == true {
-		resources.Service = true
-	}
-	if parameters.allowContainerSignedResourceType == true {
-		resources.Container = true
-	}
-	if parameters.allowObjectSignedResourceType == true {
-		resources.Object = true
-	}
-
-	permission := azblob.AccountSASPermissions{}
-	if parameters.enableList == true {
-		permission.List = true
-	}
-	if parameters.enableRead == true {
-		permission.Read = true
-	}
-	if parameters.enableWrite == true {
-		permission.Write = true
-	}
-	if parameters.enablePermanentDelete == true {
-		permission.Delete = true
-	}
-
-	start := time.Now()
-	expiry := start.AddDate(0, 0, parameters.signedExpiry)
-	sasURL, err := serviceClient.GetSASURL(resources, permission, start, expiry)
-	if err != nil {
-		return "", err
-	}
-	return sasURL, nil
 }
